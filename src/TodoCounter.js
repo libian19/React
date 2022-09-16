6bb4e9c5593728ba4ce2e2c0e8e0f8e479fde798
libian19 <libiansimpson19@gmail.com>
2022-09-15 22:35:07 -0400

    Creacion de los componentes con sus primeros estilos

---
+++ src/TodoCounter.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './TodoCounter.css';
+
+
+function TodoCounter(){
+    return(
+        <h2 className="TodoCounter">Has Completado 2 de 3 TODOs</h2>
+    )
+}
+
+export { TodoCounter };
---
+++ src/TodoList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./TodoList.css";
+//import { TodoItem } from "./TodoItem";
+
+function TodoList(props){
+    return(
+        <section>
+            <ul className="TodoList">
+                {props.children}
+            </ul>
+        </section>
+    ); 
+}
+
+export { TodoList };
